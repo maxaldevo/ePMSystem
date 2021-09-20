f7cf5649b0a540b633f82bded94b6fa11cd014d9
maxaldevo <m.corresponds@gmail.com>
2021-09-20 15:24:36 +0300

    booking manager class

---
+++ ePM.Dal/Logic/BookingManaer.cs
@@ -0,0 +1,42 @@
+﻿using ePM_Dal.Logic;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Objects;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ePM.Dal.Logic
+{
+    public static class BookingManaer
+    {
+        public static string CreateSchedule(string clinicName, int hospitalID, int userid)
+        {
+            string friendlyMsg = "";
+            try
+            {
+                using (var db = new eMedicalEntities())
+                {
+                    var outputMsgParameter = new ObjectParameter("msg", typeof(string));
+                    // db.sp_lms_addNewUser(fname, firstName, lastName, email, mobile, empNo, position, dept, roleId, hrRoleId, outputMsgParameter);
+                    db.sp_eMedical_addNewClinic(clinicName, hospitalID, userid, outputMsgParameter);
+                    friendlyMsg = outputMsgParameter.Value.ToString();
+                    //make default password 111111
+                }
+            }
+            catch (Exception ex)
+            {
+
+                friendlyMsg = "Please contact your admin!. unexpected error";
+                ExceptionsManager.AddException(ex);
+                if (ex.InnerException != null)
+                {
+                    ExceptionsManager.AddException(ex.InnerException);
+                }
+            }
+
+
+            return friendlyMsg;
+        }
+    }
+}
