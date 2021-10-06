8ce31888c7e1b3d8febe452229198774da326064
maxaldevo <m.corresponds@gmail.com>
2021-10-06 05:02:35 +0300

    test scheduling

---
+++ eMedicalSystem/eMedicalSys/testpage.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DevExpress.Web.Demos;
+
+namespace eMedicalSys
+{
+    public partial class testpage : System.Web.UI.Page
+    {
+        protected void Page_Init()
+        {
+            //DemoHelper.Instance.ControlAreaMaxWidth = Unit.Percentage(100);
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //SchedulerDemoUtils.ApplyDefaults(this, ASPxScheduler1);
+            //DataHelper.SetupDefaultMappings(ASPxScheduler1);
+            //DataHelper.ProvideRowInsertion(ASPxScheduler1, SqlDataSource1);
+            //DataHelper.SetupStatuses(ASPxScheduler1);
+            //DataHelper.SetupLabels(ASPxScheduler1);
+            //if (!IsPostBack)
+            //{
+            //    ASPxScheduler1.DayView.FirstVisibleResourceIndex = 2;
+            //    ASPxScheduler1.WorkWeekView.FirstVisibleResourceIndex = 2;
+            //}
+            //ASPxScheduler1.DataBind();
+        }
+    }
+}
