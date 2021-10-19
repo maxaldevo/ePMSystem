b1c8b364848dd29dc03ab20aa4d8da6f72a33105
Maxaldevo <m.corresponds@gmail.com>
2021-10-19 08:02:15 +0300

    added login, profile, booking, reg controls

---
+++ ePMSystemAPI/ePMSystemAPI/ePMSysAPI/Controllers/ProfileController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ePMSysAPI.Controllers
+{
+    public class ProfileController : ApiController
+    {
+        // GET: api/Profile
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Profile/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Profile
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Profile/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Profile/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ePMSystemAPI/ePMSystemAPI/ePMSysAPI/Controllers/RegistrationController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ePMSysAPI.Controllers
+{
+    public class RegistrationController : ApiController
+    {
+        // GET: api/Registration
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Registration/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Registration
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Registration/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Registration/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
